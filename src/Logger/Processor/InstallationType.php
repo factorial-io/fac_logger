55fa38d24d89fc48951ba23baa10e6d392cacdb1
Stephan Huber <stephan@factorial.io>
2022-01-13 15:48:54 +0100

    Do not log the complete stacktrace with arguments

--- src/Logger/Processor/InstallationType.php
+++ src/Logger/Processor/InstallationType.php
@@ -17,12 +17,21 @@ class InstallationType {
    *   The result.
    */
   public function __invoke(array $record) {
+
+    // Remove any args from a possible backtrace:
+    if (isset($record['context']['backtrace']) && is_array($record['context']['backtrace'])) {
+      foreach (array_keys($record['context']['backtrace']) as $ndx) {
+        unset($record['context']['backtrace'][$ndx]['args']);
+        unset($record['context']['backtrace'][$ndx]['object']);
+      }
+    }
+
     $record['extra']['installation_type'] = \Drupal::state()->get('installation_type', 'unknown');
-   
+
     if ($installation_name = \Drupal::state()->get('installation_name')) {
       $record['extra']['installation_name'] = $installation_name;
     }
-    
+
     if ($phab_config_name = \Drupal::state()->get('phab_config_name')) {
       $record['extra']['phab_config_name'] = $phab_config_name;
     }
